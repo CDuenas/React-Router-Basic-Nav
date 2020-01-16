9fd897c34afa2c60102cdcb3ec3b879d03d12563
Christian <Chrisaduenas@gmail.com>
2020-01-15 18:25:25 -0800

    mvp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div>
-    <Navigation />
+    <div>
+      <Navigation />
+    </div>
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/contact" component={Contact} />
+    </div>
   </div>
-);
+
+
+  );
 
 export default App;
